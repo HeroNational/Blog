864bd6f028d7b3a221eeb5c53bda3240ba494f2e
Kratos237 <danieluokof@gmail.com>
2020-08-19 00:51:49 +0100

    Inscription avec ajout dans la base de donnees

--- includes/connexion.php
+++ includes/connexion.php
@@ -7,8 +7,7 @@
 
     try
     {
-            $bdd = mysql_connect('localhost', 'root', '');
-            $bdd = new PDO("mysql:host=$server;dbname=$dnb", $login, $pass);
+            $bdd = new PDO("mysql:host=$server;dbname=$db", $login, $pass);
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         echo "<br><br><br><center><i class='fa fa-wrench' style='font-size:70px;'></i>  " .$e->getMessage(); 
--- pages/redirectioninscription.php
+++ pages/redirectioninscription.php
@@ -1,9 +1,11 @@
 <?php
     session_start();
+    include("../includes/connexion.php");
     $name=$_POST['name'];
     $email=$_POST['email'];
-    $password=$_POST['password'];
-    session_register("name","email","password");
+    $password=$_POST['passw'];
+    $sql="INSERT INTO inscrits (id,identifiant,email,passw) VALUES (null,'$name','$email','$password')";
+    $bdd->query($sql);
     require('../includes/connexion.php');
     header('Location: accueil.php');
 ?>
